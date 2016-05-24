09419bcf5738b372ba9cc69e57fd9e5d300cdb67
Sean Dougherty <seancdougherty@gmail.com>
2016-05-24 16:17:47 -0600

    point to staging server

--- app/src/main/java/co/ello/ElloApp/ElloURI.java
+++ app/src/main/java/co/ello/ElloApp/ElloURI.java
@@ -2,7 +2,7 @@ package co.ello.ElloApp;
 
 public class ElloURI {
     public static boolean shouldLoadInApp(String url) {
-        if (url.matches("^(https?://)?((w{3}|preview.)?ello.co|ello-webapp-(epic|rainbow).herokuapp.com)/?\\S*$")) {
+        if (url.matches("^(https?://)?((w{3}|preview.)?ello.co|(ello-webapp-epic|ello-webapp-rainbow|ello-fg-stage1|ello-fg-stage2).herokuapp.com)/?\\S*$")) {
             return true;
         }
         else if (url.matches("/\\S*$")) {
--- app/src/main/java/co/ello/ElloApp/MainActivity.java
+++ app/src/main/java/co/ello/ElloApp/MainActivity.java
@@ -23,10 +23,16 @@ public class MainActivity
         extends ActionBarActivity
         implements SwipeRefreshLayout.OnRefreshListener
 {
+    private final static String TAG = MainActivity.class.getSimpleName();
 
-    private XWalkView mWebView;
-    private SwipeRefreshLayout mSwipeLayout;
-    private String path = "https://preview.ello.co";
+    @Inject
+    protected Reachability reachability;
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    public XWalkView xWalkView;
+    private SwipeRefreshLayout swipeLayout;
+//    private String path = "http://10.1.10.153:6660";
+    public String path = "https://ello-fg-stage1.herokuapp.com ";
     private ProgressDialog progress;
     private Boolean shouldReload = false;
 
